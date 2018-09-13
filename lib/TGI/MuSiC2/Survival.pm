e145e93dca0c073473e56178f2c014b517352379
Beifang <beifang.cn@gmail.com>
2018-09-13 16:58:50 +0800

    improve survival module

--- lib/TGI/MuSiC2/Survival.pm
+++ lib/TGI/MuSiC2/Survival.pm
@@ -1,4 +1,4 @@
-package TGI::Music2::Survival;
+package TGI::MuSiC2::Survival;
 
 use warnings;
 use strict;
@@ -7,6 +7,7 @@ use Carp;
 use IO::File;
 use POSIX qw( WIFEXITED );
 use File::Temp qw/ tempfile /;
+use Getopt::Long;
 
 our $VERSION = 'v0.1';
 
@@ -69,8 +70,8 @@ sub process {
     }
     # load clinical data and analysis types
     my %clinical_data;
-    if ($this->{'numeric_clinical_data_file'}} { $clinical_data{'numeric'} = $this->{'numeric_clinical_data_file'} }
-    if ($this->{'categorical_clinical_data_file'}) { $clinical_data{'categ'} = $self->{'categorical_clinical_data_file'} }
+    if ($this->{'numeric_clinical_data_file'}) { $clinical_data{'numeric'} = $this->{'numeric_clinical_data_file'} }
+    if ($this->{'categorical_clinical_data_file'}) { $clinical_data{'categ'} = $this->{'categorical_clinical_data_file'} }
     if ($this->{'glm_clinical_data_file'}) { $clinical_data{'glm'} = $this->{'glm_clinical_data_file'} }
     # create array of all sample names possibly included from clinical data and MAF
     my $sampleFh = IO::File->new( $this->{'bam_list'} ) or die "Couldn't open $this->{'bam_list'}. $!\n";
@@ -144,9 +145,9 @@ sub process {
     map { push @mutated_genes_to_include, $_ unless exists $clinical_pheno_to_include{$_}; } @phenotypes_to_include;
     my $mutated_genes_to_include = \@mutated_genes_to_include;
     # create mutation matrix file
-    if ( $genetic_data_type =~ /^gene$/i ) {
+    if ( $this->{'genetic_data_type'} =~ /^gene$/i ) {
         $this->create_sample_gene_matrix_gene( $mutation_matrix, $mutated_genes_to_include, @all_sample_names );
-    } elsif ( $genetic_data_type =~ /^variant$/i ) {
+    } elsif ( $this->{'genetic_data_type'} =~ /^variant$/i ) {
         $this->create_sample_gene_matrix_variant( $mutation_matrix, $mutated_genes_to_include, @all_sample_names );
     } else {
         warn( "Please enter either \"gene\" or \"variant\" for the --genetic-data-type parameter." );
@@ -159,7 +160,7 @@ sub process {
         unless(mkdir $output_dir) { warn( "Failed to create output directory: $!" ); return; }
     }
     # set up R command
-    my $R_cmd = "R --slave --args < " . __FILE__ . ".R " . join( " ", $survival_data_file, $mutation_matrix, $legend_placement, $output_dir );
+    my $R_cmd = "R --slave --args < " . __FILE__ . ".R " . join( " ", $survival_data_file, $mutation_matrix, $this->{'legend_placement'}, $output_dir );
     print "R_cmd:\n$R_cmd\n";
     #run R command
     WIFEXITED( system $R_cmd ) or croak "Couldn't run: $R_cmd ($?)";
@@ -202,12 +203,19 @@ sub create_sample_gene_matrix_gene {
     my $header = join( "\t", "Sample", @all_genes );
     $matrix_fh->print( "$header\n" );
     #print mutation relation input matrix
-    map { 
-        my $sample = $_; 
+    
+    #print mutation relation input matrix
+    for my $sample ( sort @all_sample_names ) {
         $matrix_fh->print( $sample );
-        map { if ( exists $mutations{$sample}{$gene} ) { $matrix_fh->print( "\tMutated" ); } else { $matrix_fh->print( "\tWildtype" ); } } @all_genes;
+        for my $gene ( @all_genes ) {
+            if( exists $mutations{$sample}{$gene} ) {
+                $matrix_fh->print( "\tMutated" );
+            } else {
+                $matrix_fh->print( "\tWildtype" );
+            }
+        }
         $matrix_fh->print( "\n" );
-    } sort @all_sample_names;
+    }
 
 }
 
@@ -229,10 +237,10 @@ sub create_sample_gene_matrix_variant {
             return;
         }
         #check to see if this gene is on the list (if there is a list at all)
-        if ( defined @{$mutated_genes_to_include} ) { next unless (scalar grep { m/^$gene$/ } @{$mutated_genes_to_include}); }
+        if ( @{$mutated_genes_to_include} ) { next unless (scalar grep { m/^$gene$/ } @{$mutated_genes_to_include}); }
         # If user wants, skip Silent mutations, or those in Introns, RNA, UTRs, Flanks, IGRs, or the ubiquitous Targeted_Region
-        if (( $self->skip_non_coding && $mutation_class =~ m/^(Intron|RNA|3'Flank|3'UTR|5'Flank|5'UTR|IGR|Targeted_Region)$/ ) ||
-           ( $self->skip_silent && $mutation_class =~ m/^Silent$/ )) {
+        if (( $this->skip_non_coding && $mutation_class =~ m/^(Intron|RNA|3'Flank|3'UTR|5'Flank|5'UTR|IGR|Targeted_Region)$/ ) ||
+           ( $this->skip_silent && $mutation_class =~ m/^Silent$/ )) {
             print "Skipping $mutation_class mutation in gene $gene.\n";
             next;
         }
@@ -278,7 +286,127 @@ sub create_sample_gene_matrix_variant {
         }
         $matrix_fh->print("\n");
     }
+}
+
+
+## usage
+sub help_text {
+    my $this = shift;
+        return <<HELP
+
+USAGE
+
+    music2 survival --bam-list=? --output-dir=? [--maf-file=?] [--skip-silent]
+    [--genetic-data-type=?] [--numeric-clinical-data-file=?] [--categorical-clinical-data-file=?]
+    [--glm-clinical-data-file=?] [--phenotypes-to-include=?] [--legend-placement=?]
+    [--skip-non-coding]
+
+SYNOPSIS
+
+    music2 survival \
+    --bam-list /path/myBamList.tsv \
+    --maf-file /path/myMAF.tsv \
+    --numeric-clinical-data-file /path/myNumericData.tsv \
+    --categorical-clinical-data-file /path/myClassData.tsv \
+    --output-dir /path/output_directory
+
+    music2 survival \
+    --bam-list /path/myBamList.tsv \
+    --maf-file /path/myMAF.tsv \
+    --glm-clinical-data-file /path/myGLMClinicalData.tsv \
+    --output-dir /path/output_directory
+
+    music2 survival \
+    --bam-list /path/myBamList.tsv \
+    --maf-file /path/myMAF.tsv \
+    --genetic-data-type 'gene' \
+    --glm-clinical-data-file /path/myGlmClinicalData.tsv \
+    --phenotypes-to-include 'Race,Gender,TP53' \
+    --output-dir /path/output_directory
+
+REQUIRED INPUTS
+
+    bam-list
+      List of sample names to be included in the analysis. (See Description) 
+    output-dir
+      Directory where output files will be written 
+
+OPTIONAL INPUTS
+
+    maf-file
+      List of mutations in MAF format 
+    skip-silent
+      Skip silent mutations from the provided MAF file 
+      Default value 'true' if not specified
+    noskip-silent
+      Make skip-silent 'false' 
+    genetic-data-type
+      Correlate clinical data to "gene" or "variant" level data 
+      Default value 'gene' if not specified
+    numeric-clinical-data-file
+      Table of samples (y) vs. numeric clinical data category (x) 
+    categorical-clinical-data-file
+      Table of samples (y) vs. categorical clinical data category (x) 
+    glm-clinical-data-file
+      Clinical traits, mutational profiles, other mixed clinical data (See DESCRIPTION). 
+    phenotypes-to-include
+      Include only these genes and/or phenotypes in the anlaysis. (COMMA-DELIMITED) 
+    legend-placement
+      Choose one of 'bottomleft', 'topleft', 'topright', or 'bottomright'. 
+      Default value 'bottomleft' if not specified
+    skip-non-coding
+      Skip non-coding mutations from the provided MAF file 
+      Default value 'true' if not specified
+    noskip-non-coding
+      Make skip-non-coding 'false' 
+
+DESCRIPTION
+
+    Create survival plots and P-values for clinical and mutational phenotypes.Create survival 
+    plots and P-values for clinical and mutational phenotypes.
+
+    This command performs survival analysis and plots survival curves for mutational data, as well
+    as any clinical traits of interest as specified via the --phenotypes-to-include input
+    parameter. The analyses performed include the Kaplan-Meier estimator followed by the Cox
+    Proportional Hazards model. Outputs for each gene/clinical trait analyzed include survival
+    curves, a hazard ratio (with confidence intervals), and P-values and FDRs describing the
+    significance of the difference between survivors and non-survivors.
+
+    All clinical data files are searched for the required (case insensitive) "vital_status" and
+    "days_to_last_followup" columns which are paired to phenotypes via sample IDs for the survival
+    analysis. The first column of all clinical data files MUST contain the sample IDs, same as in
+    other MuSiC tools. By default, analysis is performed on every gene present in the MAF.
+    Optionally, the analysis may be limited to only specific genes by listing them (comma
+    delimited) after the --phenotypes-to-include input parameter. Survival analysis may also be
+    performed on other columns in the clinical data file by adding the column headers to the list
+    of entries specified after the --phenotypes-to-include input parameter.
+
+    Here are some general guildelines for creating clinical data input files:
+
+    * Headers are required.
+
+    * The first column of each clinical data file must contain sample IDs which match those in
+    both the --bam-list and the MAF variant list (in the MAF, this is the Tumor_Sample_Barcode
+    column, specifically).
+
+    * In at least one of the clinical data files input, columns with headers "vital_status" and
+    "days_to_last_followup" (case insensitive) must exist. "vital_status" must be delineated by
+    1's and 0's, where 0 denotes 'living', and 1 denotes 'deceased'.
+
+    Note that all input files must be tab-separated.
+
+ARGUMENTS
+
+    --bam-list
+
+      Provide a file containing sample names and normal/tumor BAM locations for each. Use the tab-
+      delimited format [sample_name normal_bam tumor_bam] per line. This tool only needs
+      sample_name, so all other columns can be skipped. The sample_name must be the same as the
+      tumor sample names used in the MAF file (16th column, with the header Tumor_Sample_Barcode).
+
+HELP
 
 }
 
 1;
+
